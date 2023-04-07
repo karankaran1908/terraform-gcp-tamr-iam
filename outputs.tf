191828bfe38cd7163b4458a52c3d86d4bf1e7505
creynolds <colin.reynolds@tamr.com>
2023-04-07 07:48:02 -0700

    fix: output var service account instead of local.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "service_account_email" {
-  value       = local.tamr_service_account
+  value       = var.tamr_service_account
   description = "Email of the service account given permissions to"
 }
